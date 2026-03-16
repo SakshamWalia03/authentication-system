364b0324655a7ea8e7f90aac016ba108b61fa209
SakshamWalia03 <wsaksham37@gmail.com>
2026-03-16 22:55:25 +0530

    refactor(email): switch Nodemailer auth from OAuth2 to Gmail App Password

--- src/services/sendEmail.js
+++ src/services/sendEmail.js
@@ -4,11 +4,8 @@ import config from "../config/config.js";
 const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    type: "OAuth2",
     user: config.GOOGLE_USER,
-    clientId: config.GOOGLE_CLIENT_ID,
-    clientSecret: config.GOOGLE_CLIENT_SECRET,
-    refreshToken: config.GOOGLE_REFRESH_TOKEN,
+    pass: config.GOOGLE_APP_PASSWORD,
   },
 });
 
@@ -32,7 +29,6 @@ export const sendEmail = async (to, subject, text, html) => {
     });
 
     console.log("Message sent: %s", info.messageId);
-    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
   } catch (error) {
     console.error("Error sending email:", error);
   }
